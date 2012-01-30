1919eac454893b0c51fb519c3898b21e7af99ccb
Marko Gargenta <marko@marakana.com>
2012-01-30 15:41:01 -0800

    Initial checkin

---
+++ Yamba/src/com/marakana/android/yamba/StatusActivity.java
@@ -0,0 +1,15 @@
+package com.marakana.android.yamba;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class StatusActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.status);
+        
+    }
+}
