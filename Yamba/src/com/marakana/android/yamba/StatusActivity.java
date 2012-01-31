2bb1aa1e9a28188cdefcce43c45e61a77fa76170
Marko Gargenta <marko@marakana.com>
2012-01-31 11:01:59 -0800

    Added icons, changed title and theme

--- Yamba/src/com/marakana/android/yamba/StatusActivity.java
+++ Yamba/src/com/marakana/android/yamba/StatusActivity.java
@@ -2,7 +2,8 @@ package com.marakana.android.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class StatusActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,6 +11,5 @@ public class StatusActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.status);
-        
     }
 }
