21b30675b4a6cc0896189374fc0fe16d2f771142
Marko Gargenta <marko@marakana.com>
2012-01-31 13:04:20 -0800

    Added button and onClick() to activity

--- Yamba/src/com/marakana/android/yamba/StatusActivity.java
+++ Yamba/src/com/marakana/android/yamba/StatusActivity.java
@@ -2,14 +2,32 @@ package com.marakana.android.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener {
+	Button buttonUpdate;
+	EditText editStatus;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.status);
+        
+        // Find views
+        buttonUpdate = (Button) findViewById(R.id.button_update);
+        editStatus = (EditText)findViewById(R.id.edit_status);
+        
+        // Add button listener
+        buttonUpdate.setOnClickListener(this);
     }
+
+	@Override
+	public void onClick(View v) {
+		Log.d("Yamba", "Status update button clicked");
+	}
 }
