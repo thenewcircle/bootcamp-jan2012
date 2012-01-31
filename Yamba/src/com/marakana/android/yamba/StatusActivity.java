64e4932759fa54310ae05e52b908b4291cfb237b
Marko Gargenta <marko@marakana.com>
2012-01-31 15:24:47 -0800

    Added Toast

--- Yamba/src/com/marakana/android/yamba/StatusActivity.java
+++ Yamba/src/com/marakana/android/yamba/StatusActivity.java
@@ -8,26 +8,27 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class StatusActivity extends Activity implements OnClickListener {
 	Button buttonUpdate;
 	EditText editStatus;
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.status);
-        
-        // Find views
-        buttonUpdate = (Button) findViewById(R.id.button_update);
-        editStatus = (EditText) findViewById(R.id.edit_status);
-        
-        // Add button listener
-        buttonUpdate.setOnClickListener(this);
-    }
 
-    /** Called when the update button is clicked. */
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.status);
+
+		// Find views
+		buttonUpdate = (Button) findViewById(R.id.button_update);
+		editStatus = (EditText) findViewById(R.id.edit_status);
+
+		// Add button listener
+		buttonUpdate.setOnClickListener(this);
+	}
+
+	/** Called when the update button is clicked. */
 	@Override
 	public void onClick(View v) {
 		final String status = editStatus.getText().toString();
@@ -36,10 +37,13 @@ public class StatusActivity extends Activity implements OnClickListener {
 			public void run() {
 				Twitter twitter = new Twitter("student", "password");
 				twitter.setAPIRootUrl("http://yamba.marakana.com/api");
-				twitter.setStatus(status);				
-				Log.d("Yamba", "onClicked with status: "+status);
+				twitter.setStatus(status);
+				Log.d("Yamba", "onClicked with status: " + status);
+				Toast.makeText(StatusActivity.this,
+						"Successfully posted: " + status, Toast.LENGTH_LONG)
+						.show();
 			}
 		}.start();
-		
+
 	}
 }
